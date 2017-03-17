d56efebeecc8bcdf79811ec3c0c6eaa7b43bb761
isclouder <isclouder@163.com>
2017-03-17 16:19:23 +0800

    add client

---
+++ client.py
@@ -0,0 +1,92 @@
+
+import json
+import urllib2
+
+class Opthttp(object):
+    def __init__(self, passwd):
+        self.token = None
+        self.tenant = None
+        self.token, self.tenant = self.__get_token(passwd)
+
+    def __get_token(self, passwd):
+        url = 'http://127.0.0.1:5000/v2.0/tokens'
+        values = {"auth": {"tenantName": "admin", "passwordCredentials": {"username": "admin", "password": passwd}}}
+        data = self.http_post(url, values)
+        token = data['access']['token']['id']
+        tenant = data['access']['token']['tenant']['id']
+        return token, tenant
+
+    def http_get(self, url):
+        req = urllib2.Request(url)
+        req.add_header('X-Auth-Token', self.token)
+        response = urllib2.urlopen(req)
+        data = response.read()
+        data = json.loads(data)
+        return data
+
+    def _do_http_post(self, method, url, body):
+        if body:
+            jdata = json.dumps(body)
+            req = urllib2.Request(url, jdata)
+        else:
+            req = urllib2.Request(url, None)
+        req.add_header('Content-Type', 'application/json')
+        req.add_header('Accept', 'application/json')
+        if self.token:
+            req.add_header('X-Auth-Token', self.token)
+        req.get_method = lambda: method
+        response = urllib2.urlopen(req)
+        data = response.read()
+        if data != '':
+            data = json.loads(data)
+        return data
+
+    def http_post(self, url, body):
+        return self._do_http_post('POST', url, body)
+
+    def http_put(self, url, body):
+        return self._do_http_post('PUT', url, body)
+
+    def http_delete(self, url, body):
+        return self._do_http_post('DELETE', url, body)
+
+class Neutron(object):
+    def __init__(self):
+        self.baseurl = 'http://cvm:9696/v2.0'
+        self.http = Opthttp(config.ADMIN_PASS)
+
+    def router_list(self):
+        url = '%s/routers.json' %(self.baseurl)
+        list = self.http.http_get(url)
+        return list
+
+    def router_port_list(self, id):
+        url = '%s/ports.json?device_id=%s' %(self.baseurl, id)
+        list = self.http.http_get(url)
+        return list
+
+    def router_create(self, name):
+        url = '%s/routers.json' %(self.baseurl)
+        body = {'router': {'name': name, 'admin_state_up': True}}
+        ret = self.http.http_post(url, body)
+        return ret
+
+    def router_gateway_clear(self, id):
+        url = '%s/routers/%s.json' %(self.baseurl, id)
+        body = {'router': {'external_gateway_info': {}}}
+        ret = self.http.http_put(url, body)
+        return ret
+
+    def router_interface_delete(self, id, subnet_id):
+        url = '%s/routers/%s/remove_router_interface.json' %(self.baseurl, id)
+        body = {'subnet_id': subnet_id}
+        ret = self.http.http_put(url, body)
+        return ret
+
+    def router_delete(self, id):
+        url = '%s/routers/%s.json' %(self.baseurl, id)
+        body = None
+        ret = self.http.http_delete(url, body)
+        return ret
+
+
--- deploy.py
+++ deploy.py
@@ -1,51 +1,35 @@
-#!/usr/bin/python
 
 import os
 import commands
 
-CONTROLLER_HOSTNAME='controller'
-ADMIN_PASS='123456'
-MYSQL_PASS='123456'
-GLANCE_PASS = '123456'
-NOVA_PASS = '123456'
-NEUTRON_PASS = '123456'
-CINDER_PASS = '123456'
-
-KEYSTONE_DBPASS = '123456'
-GLANCE_DBPASS = '123456'
-NOVAAPI_DBPASS = '123456'
-NOVA_DBPASS = '123456'
-NEUTRON_DBPASS = '123456'
-CINDER_DBPASS = '123456'
-
 dbopt = {
-         "keystone":  [{"sysuser": "keystone",  "dbname": "keystone",  "dbpasswd": KEYSTONE_DBPASS,  "dbsync": "keystone-manage db_sync"}],
-         "glance":    [{"sysuser": "glance",    "dbname": "glance",    "dbpasswd": GLANCE_DBPASS,    "dbsync": "glance-manage db_sync"}],
+         "keystone":  [{"sysuser": "keystone",  "dbname": "keystone",  "dbpasswd": config.KEYSTONE_DBPASS,  "dbsync": "keystone-manage db_sync"}],
+         "glance":    [{"sysuser": "glance",    "dbname": "glance",    "dbpasswd": config.GLANCE_DBPASS,    "dbsync": "glance-manage db_sync"}],
          "nova":      [
-                       {"sysuser": "nova",      "dbname": "nova_api",  "dbpasswd": NOVAAPI_DBPASS,   "dbsync": "nova-manage api_db sync"},
-                       {"sysuser": "nova",      "dbname": "nova",      "dbpasswd": NOVA_DBPASS,      "dbsync": "nova-manage db sync"}
+                       {"sysuser": "nova",      "dbname": "nova_api",  "dbpasswd": config.NOVAAPI_DBPASS,   "dbsync": "nova-manage api_db sync"},
+                       {"sysuser": "nova",      "dbname": "nova",      "dbpasswd": config.NOVA_DBPASS,      "dbsync": "nova-manage db sync"}
                       ],
-         "neutron":   [{"sysuser": "neutron",   "dbname": "neutron",   "dbpasswd": NEUTRON_DBPASS,   "dbsync": "neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head"}],
-         "cinder":    [{"sysuser": "cinder",    "dbname": "cinder",    "dbpasswd": CINDER_DBPASS,    "dbsync": "cinder-manage db sync"}]
+         "neutron":   [{"sysuser": "neutron",   "dbname": "neutron",   "dbpasswd": config.NEUTRON_DBPASS,   "dbsync": "neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head"}],
+         "cinder":    [{"sysuser": "cinder",    "dbname": "cinder",    "dbpasswd": config.CINDER_DBPASS,    "dbsync": "cinder-manage db sync"}]
         }
 
 serviceopt ={
-              "glance": [{"name":"glance",   "type":"image",    "description":"OpenStack Image",         "http":"http://%s:9292" % CONTROLLER_HOSTNAME}],
-              "nova":   [{"name":"nova",     "type":"compute",  "description":"OpenStack Compute",       "http":"http://%s:8774/v2.1/%%\(tenant_id\)s" % CONTROLLER_HOSTNAME}],
-              "neutron":[{"name":"neutron",  "type":"network",  "description":"OpenStack Networking",    "http":"http://%s:9696" % CONTROLLER_HOSTNAME}],
+              "glance": [{"name":"glance",   "type":"image",    "description":"OpenStack Image",         "http":"http://%s:9292" % config.CONTROLLER_HOSTNAME}],
+              "nova":   [{"name":"nova",     "type":"compute",  "description":"OpenStack Compute",       "http":"http://%s:8774/v2.1/%%\(tenant_id\)s" % config.CONTROLLER_HOSTNAME}],
+              "neutron":[{"name":"neutron",  "type":"network",  "description":"OpenStack Networking",    "http":"http://%s:9696" % config.CONTROLLER_HOSTNAME}],
               "cinder": [
-                         {"name":"cinder",   "type":"volume",   "description":"OpenStack Block Storage", "http":"http://%s:8776/v1/%%\(tenant_id\)s" % CONTROLLER_HOSTNAME},
-                         {"name":"cinderv2", "type":"volumev2", "description":"OpenStack Block Storage", "http":"http://%s:8776/v2/%%\(tenant_id\)s" % CONTROLLER_HOSTNAME}
+                         {"name":"cinder",   "type":"volume",   "description":"OpenStack Block Storage", "http":"http://%s:8776/v1/%%\(tenant_id\)s" % config.CONTROLLER_HOSTNAME},
+                         {"name":"cinderv2", "type":"volumev2", "description":"OpenStack Block Storage", "http":"http://%s:8776/v2/%%\(tenant_id\)s" % config.CONTROLLER_HOSTNAME}
                         ]
              }
 
 admin_env ={
              "OS_USERNAME": "admin",
-             "OS_PASSWORD": ADMIN_PASS,
+             "OS_PASSWORD": config.ADMIN_PASS,
              "OS_PROJECT_NAME": "admin",
              "OS_USER_DOMAIN_NAME": "Default",
              "OS_PROJECT_DOMAIN_NAME": "Default",
-             "OS_AUTH_URL": "http://%s:35357/v3" % CONTROLLER_HOSTNAME,
+             "OS_AUTH_URL": "http://%s:35357/v3" % config.CONTROLLER_HOSTNAME,
              "OS_IDENTITY_API_VERSION": "3",
              "OS_IMAGE_API_VERSION": "2"
            }
@@ -60,7 +44,7 @@ def create_db(dbs):
     for db in dbs:
         try:
             sql = "DROP DATABASE %s; " % db['dbname']
-            cmd = "mysql -uroot -p%s -e \"%s\"" % (MYSQL_PASS, sql)
+            cmd = "mysql -uroot -p%s -e \"%s\"" % (config.MYSQL_PASS, sql)
             exec_cmd(cmd)
         except Exception as e:
             pass
@@ -68,7 +52,7 @@ def create_db(dbs):
         sql = "CREATE DATABASE %s; " % db['dbname'] + \
               "GRANT ALL PRIVILEGES ON %s.* TO '%s'@'localhost' IDENTIFIED BY '%s'; " % (db['dbname'], db['sysuser'], db['dbpasswd']) + \
               "GRANT ALL PRIVILEGES ON %s.* TO '%s'@'%%' IDENTIFIED BY '%s'" % (db['dbname'], db['sysuser'], db['dbpasswd'])
-        cmd = "mysql -uroot -p%s -e \"%s\"" % (MYSQL_PASS, sql)
+        cmd = "mysql -uroot -p%s -e \"%s\"" % (config.MYSQL_PASS, sql)
         exec_cmd(cmd)
 
         cmd = "su -s /bin/sh -c \"%s\" %s" % (db['dbsync'], db['sysuser'])
@@ -78,10 +62,10 @@ def create_db(dbs):
 def keystone_init():
     cmd = "keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone; " + \
           "keystone-manage credential_setup --keystone-user keystone --keystone-group keystone; " + \
-          "keystone-manage bootstrap --bootstrap-password %s " % ADMIN_PASS + \
-          "--bootstrap-admin-url http://%s:35357/v3/ " % CONTROLLER_HOSTNAME + \
-          "--bootstrap-internal-url http://%s:35357/v3/ " % CONTROLLER_HOSTNAME + \
-          "--bootstrap-public-url http://%s:5000/v3/ " % CONTROLLER_HOSTNAME + \
+          "keystone-manage bootstrap --bootstrap-password %s " % config.ADMIN_PASS + \
+          "--bootstrap-admin-url http://%s:35357/v3/ " % config.CONTROLLER_HOSTNAME + \
+          "--bootstrap-internal-url http://%s:35357/v3/ " % config.CONTROLLER_HOSTNAME + \
+          "--bootstrap-public-url http://%s:5000/v3/ " % config.CONTROLLER_HOSTNAME + \
           "--bootstrap-region-id RegionOne"
     exec_cmd(cmd)
 
@@ -113,34 +97,16 @@ def print_result():
           "openstack endpoint list "
     os.system(cmd)
 
-def restart_service():
-    cmd = "service glance-registry restart; " + \
-          "service glance-api restart; " + \
-          "service nova-api restart; " + \
-          "service nova-consoleauth restart; " + \
-          "service nova-scheduler restart; " + \
-          "service nova-conductor restart; " + \
-          "service nova-novncproxy restart; " + \
-          "service nova-api restart; " + \
-          "service neutron-server restart; " + \
-          "service neutron-linuxbridge-agent restart; " + \
-          "service neutron-dhcp-agent restart; " + \
-          "service neutron-metadata-agent restart; " + \
-          "service neutron-l3-agent restart; " + \
-          "service cinder-scheduler restart; " + \
-          "service cinder-api restart "
-    exec_cmd(cmd)
-
-def REST(name):
+def deploy(name):
     if name == 'all':
         create_db(dbopt['keystone'])
         keystone_init()
         os.environ.update(admin_env)
         create_project_service()
-        create_user_and_service('glance', GLANCE_PASS)
-        create_user_and_service('nova', NOVA_PASS)
-        create_user_and_service('neutron', NEUTRON_PASS)
-        create_user_and_service('cinder', CINDER_PASS)
+        create_user_and_service('glance', config.GLANCE_PASS)
+        create_user_and_service('nova', config.NOVA_PASS)
+        create_user_and_service('neutron', config.NEUTRON_PASS)
+        create_user_and_service('cinder', config.CINDER_PASS)
 
     os.environ.update(admin_env)
     if name == 'all' or name == 'glance':
@@ -152,8 +118,5 @@ def REST(name):
     if name == 'all' or name == 'cinder':
         create_db(dbopt['cinder'])
 
-
-    restart_service()
     print_result()
 
-REST('all')
